0034ffee525ba469254e13ef0f3b3521be031a53
Simone823 <simone.daglio823@outlook.it>
2022-10-26 12:59:42 +0200

    install boostrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 // import app provider
 import {AppProvider} from './context/AppProvider';
