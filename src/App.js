92015bf6dc58dfc7ac232780cf58bdfea880ecb5
Simone823 <simone.daglio823@outlook.it>
2022-10-26 15:53:29 +0200

    import products

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 // import header
 import Header from './components/Header';
 
+// import products
+import Products from './components/Products';
+
 function App() {
   return (
     <div className="App">
       {/* Header */}
       <Header/>
+
+      {/* main */}
+      <main>
+        {/* products */}
+        <Products/>
+      </main>
     </div>
   );
 }
