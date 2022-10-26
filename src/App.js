93e03a59b4dba696cff276c6bec6a8465e5e4d8f
Simone823 <simone.daglio823@outlook.it>
2022-10-26 13:30:21 +0200

    import header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+// import header
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-
+      {/* Header */}
+      <Header/>
     </div>
   );
 }
