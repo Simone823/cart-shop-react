975aff6861df05906b16e532cef15e445afd7d55
Simone823 <simone.daglio823@outlook.it>
2022-10-26 13:30:00 +0200

    get data axios app provder

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -1,13 +1,60 @@
-import React, {createContext} from 'react';
+import React, {createContext, useReducer, useEffect} from 'react';
+
+// import reducer
+import reducer from './reducer';
+
+// import axios
+import axios from 'axios';
 
 // app context
 const AppContext = createContext();
 
+// initial state reducer
+const initialState = {
+    products: [],
+    isLoading: true,
+    isError: false
+}
+
 // componente wrap app; l'oggetto children sarà App o il componente che ha bisogno di accedere ad un determinato provider
 function AppProvider({children}) {
+
+    // use reducer state dispatch
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    // get products
+    const getProducts = () => {      
+        
+        // dispatch get products start
+        dispatch({type: 'GET_PRODUCTS_START'});
+        
+        // axios get products
+        axios.get('https://react--course-api.herokuapp.com/api/v1/data/cart')
+        .then((res) => {
+            // dispacth get products success
+            dispatch({type: 'GET_PRODUCTS_SUCCESS', payload: res.data.data});
+        })
+        .catch((err) => {
+            // dispacth get products success
+            dispatch({type: 'GET_PRODUCTS_ERROR'});
+
+            // console warn err
+            console.warn(err.message);
+        })
+    }
+
+    // use effect get products
+    useEffect(() => {
+        getProducts();
+    }, []);
+
     return (
         <AppContext.Provider 
-            value='app context'
+            value={
+                {
+                    ...state
+                }
+            }
         >
             {children}
         </AppContext.Provider>
